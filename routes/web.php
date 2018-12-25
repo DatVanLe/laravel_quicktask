5bf814fbab8b8800cf1f34ef3b45b7fa724899e2
Your Name <vandattpm@gmail.com>
2018-12-25 16:02:25 +0700

    routing

--- routes/web.php
+++ routes/web.php
@@ -10,7 +10,5 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
-
-Route::get('/', function () {
-    return view('welcome');
-});
+//show
+Route::get('index', ['as' => 'index', 'uses' => 'quickController@getindex']);
