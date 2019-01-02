50e7009f0f55f70c93cae300838eb5d6f31dee8f
Your Name <vandattpm@gmail.com>
2019-01-02 09:35:52 +0700

    add and show

--- resources/views/tasks.blade.php
+++ resources/views/tasks.blade.php
@@ -2,6 +2,7 @@
 @section('content')
 
 <div class="pannel-body">
+	@include('errors')
 	{!! Form::open() !!}
 		<div class="form-group">
 			{!!Form::label('task', 'Task', ['class' => 'col-sm-3 control-label'])!!}
@@ -17,4 +18,33 @@
 		</div>
 	{!! Form::close() !!}
 </div>
+	@if (count($tasks) > 0)
+        <div class="panel panel-default">
+            <div class="panel-heading">
+                Current Tasks
+            </div>
+            <div class="panel-body">
+                <table class="table table-striped task-table">
+                    <thead>
+                        <th>Task</th>
+                        <th>&nbsp;</th>
+                    </thead>
+
+                    <tbody>
+                        @foreach ($tasks as $task)
+                            <tr>
+                                <td class="table-text">
+                                    <div>{{ $task->name }}</div>
+                                </td>
+
+                                <td>
+                                    <!-- TODO: Delete Button -->
+                                </td>
+                            </tr>
+                        @endforeach
+                    </tbody>
+                </table>
+            </div>
+        </div>
+    @endif
 @endsection
